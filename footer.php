244ba634857f43680f7d2223f5b0e9d017286dd2
Joonas Nuutinen <joonas@joonasnuutinen.com>
2019-02-08 08:34:54 +0200

    Remove footer content

--- footer.php
+++ footer.php
@@ -14,19 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ilona' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ilona' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ilona' ), 'ilona', '<a href="https://www.joonasnuutinen.com/">Joonas Nuutinen</a>' );
-				?>
-		</div><!-- .site-info -->
+	
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
