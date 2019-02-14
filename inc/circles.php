7269be9072c89a85df8934b99833efb80db814ae
Joonas Nuutinen <joonas@joonasnuutinen.com>
2019-02-14 08:04:10 +0200

    Style circle container

--- inc/circles.php
+++ inc/circles.php
@@ -1,9 +1,7 @@
 <?php
 function ilona_circles() {
     ?>
-    <div class="ilona-circles">
-        <div class="ilona-circle ilona-c1" style="width: 40px; height: 40px;"></div>
-    </div>
+    <div class="ilona-circles"></div>
     <?php
 }
 ?>
