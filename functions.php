47cdad77956fe0bd8e054ccbecc8902c6e732fa6
Joonas Nuutinen <joonas@joonasnuutinen.com>
2019-02-25 08:26:01 +0200

    Change style and script versions

--- functions.php
+++ functions.php
@@ -126,15 +126,15 @@ function ilona_scripts() {
 
 	wp_enqueue_style( 'crimson-text', 'https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600,600i,700,700i', array(), null );
 
-	wp_enqueue_style( 'ilona-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'ilona-style', get_stylesheet_uri(), array(),  );
 
-	wp_enqueue_script( 'ilona-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20151215', true );
+	wp_enqueue_script( 'ilona-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), false, true );
 
 	wp_enqueue_script( 'ilona-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'ilona-carousel', get_template_directory_uri() . '/js/carousel.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'ilona-carousel', get_template_directory_uri() . '/js/carousel.js', array( 'jquery' ), false, true );
 
-	wp_enqueue_script( 'ilona-circles', get_template_directory_uri() . '/js/circles.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'ilona-circles', get_template_directory_uri() . '/js/circles.js', array( 'jquery' ), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
