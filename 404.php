fcec2156f8a8842768cd254c94cf08115cf1cb5d
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-03-03 16:15:28 +0200

    Modify 404 page

--- 404.php
+++ 404.php
@@ -15,41 +15,13 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ilona' ); ?></h1>
+					<h1 class="page-title h2">Sivua ei löydy</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ilona' ); ?></p>
+					<p>Etsimääsi sivua ei löydy.</p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ilona' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$ilona_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ilona' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ilona_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					<p><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">Siirry etusivulle</a></p>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
